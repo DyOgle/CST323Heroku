78664c98e9dd5d74dd52ab1431979b4b720ceda7
DyOgle <dogle@my.gcu.edu>
2024-02-06 10:40:52 -0700

    first commit

--- src/main/java/com/gcu/HomeController.java
+++ src/main/java/com/gcu/HomeController.java
@@ -1,13 +1,15 @@
 package com.gcu;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/")
 public class HomeController 
 {
 		// Display Home Page
-		@GetMapping("/home")
+		@GetMapping("/")
 		public String showAllListings()
 		{
 			return "home";
